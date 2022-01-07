5e8011e4cfb65947cc2bb467b132f6c6aba9c152
늇뉴진 <68093782+zladb@users.noreply.github.com>
2022-01-08 00:05:15 +0900

    Add files via upload

---
+++ p_1000.py
@@ -0,0 +1,5 @@
+# 1000
+input_ = input()
+sep = input_.split(' ')
+result = int(sep[0]) + int(sep[1])
+print(result)
---
+++ p_1001.py
@@ -0,0 +1,5 @@
+# 1001
+input_ = input()
+sep = input_.split(' ')
+result = int(sep[0]) - int(sep[1])
+print(result)
