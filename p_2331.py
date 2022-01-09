863bac9968f295518b0716e1ba854d8e69bf59d5
늇뉴진 <68093782+zladb@users.noreply.github.com>
2022-01-09 19:12:58 +0900

    분해합

---
+++ p_2331.py
@@ -0,0 +1,16 @@
+# 2331 - 분해합
+
+# 자연수 n(1<=n<=1,000,000)
+n = int(input())
+result = 0
+
+for i in range(1, n):
+    str_num = list(str(i))
+    elements = [int(x) for x in str_num]
+    # print("elements : ", elements)
+    분해합 = i + sum(elements)
+    if 분해합 == n:
+        result = i
+        break
+
+print(result)
