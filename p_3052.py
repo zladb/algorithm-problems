0cdb5dd358e224df456b1352265c60f395ebd3b4
늇뉴진 <68093782+zladb@users.noreply.github.com>
2022-01-29 21:32:52 +0900

    나머지

--- p_3052.py
+++ p_3052.py
@@ -1,13 +1,12 @@
-# 나머지
+# 3052 - 나머지
 
 numbers = list()
-remainder = list()
+remainder = set()
 
 for _ in range(10):
     numbers.append(int(input()))
 
 for num in numbers:
-    if num % 42 not in remainder:
-        remainder.append(num % 42)
+    remainder.add(num % 42)
 
 print(len(remainder))
