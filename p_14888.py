098579f492b27d4f674ead5ab2a4cdae68457503
늇뉴진 <68093782+zladb@users.noreply.github.com>
2022-01-30 18:33:08 +0900

    연산자 끼워넣기

--- p_14888.py
+++ p_14888.py
@@ -1,12 +1,8 @@
 # 14888 - 연산자 끼워넣기
 
 N = int(input())
-
 numbers = list(map(int, input().split()))
-operator = list(map(int, input().split()))  # + - * /
-
-print(numbers)
-print(operator)
+operator = list(map(int, input().split()))
 
 num_len = len(numbers)
 operator_len = num_len - 1
@@ -14,14 +10,22 @@ operator_len = num_len - 1
 expression = list()
 sol = list()
 
-def dfs(i):
+def dfs(i, val):
     expression.append(str(numbers[i]))
+
+    if i != 0:
+        # 음수를 나누는 경우
+        if val < 0 and expression[-2] == '//':
+            val = -((-val) // int(expression[-1]))
+        else:
+            exp = str(val) + ''.join(expression[-2:])
+            val = eval(exp)
+
+    # 식을 완성한 경우
     if len(expression) == num_len + operator_len:
-        print(*expression, sep=' ')
-        exp = ''.join(expression)
-        print(eval(exp))
-        sol.append(eval(exp))
-        return 0
+        sol.append(val)
+        return
+
     else:
         for j in range(0, 4):
             if operator[j] != 0 and i + 1 != num_len:
@@ -35,12 +39,12 @@ def dfs(i):
                     expression.append('//')
 
                 operator[j] -= 1
-                dfs(i + 1)
+                dfs(i + 1, val)
                 expression.pop()
                 expression.pop()
                 operator[j] += 1
 
 
-dfs(0)
+dfs(0, numbers[0])
 print(max(sol))
 print(min(sol))
