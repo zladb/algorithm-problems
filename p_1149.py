22c95789253b18db6ec2b5fe527fb517721010e4
늇뉴진 <68093782+zladb@users.noreply.github.com>
2022-02-02 19:58:01 +0900

    RGB 거리

--- p_1149.py
+++ p_1149.py
@@ -7,13 +7,8 @@ for _ in range(n):
     cost.append(list(map(int, input().split())))
 
 for i in range(1, n):
-    temp = cost[i].copy()
-    for color in range(3):
-        min_list = cost[i-1].copy()
-        min_list.pop(color)
-        min_value = min(min_list)
-        temp[color] += min_value
+    cost[i][0] += min(cost[i - 1][1], cost[i - 1][2])
+    cost[i][1] += min(cost[i - 1][0], cost[i - 1][2])
+    cost[i][2] += min(cost[i - 1][0], cost[i - 1][1])
 
-    cost[i][:] = temp
-
-print(min(temp))
+print(min(cost[-1]))
