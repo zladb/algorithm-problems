ba606c1e80394ba5b349c13496c4784883045e05
늇뉴진 <68093782+zladb@users.noreply.github.com>
2022-02-02 19:03:57 +0900

    RGB 거리

--- p_1149.py
+++ p_1149.py
@@ -1,11 +1,19 @@
 # 1149 - RGB 거리
 
-from pprint import pprint
-
 n = int(input())
 
 cost = list()
 for _ in range(n):
     cost.append(list(map(int, input().split())))
 
-pprint(cost)
+for i in range(1, n):
+    temp = cost[i].copy()
+    for color in range(3):
+        min_list = cost[i-1].copy()
+        min_list.pop(color)
+        min_value = min(min_list)
+        temp[color] += min_value
+
+    cost[i][:] = temp
+
+print(min(temp))
