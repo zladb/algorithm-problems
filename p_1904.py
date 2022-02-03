4399710f4ab531744148672dc93cd7b0c26ad91d
늇뉴진 <68093782+zladb@users.noreply.github.com>
2022-02-04 03:06:36 +0900

    01타일

--- p_1904.py
+++ p_1904.py
@@ -1,4 +1,13 @@
-# 01타일
+# 1904 - 01타일
+
+def tile(n):
+    if len(sol) < n:
+        for i in range(len(sol), n + 1):
+            sol.append(sol[i - 1] + sol[i - 2])
+
+    print(sol[n - 1] % 15746)
+
 
 n = int(input())
-
+sol = [1, 2]
+tile(n)
