c092d914a62231d1b4b9e7e8f7eeda18954e378a
늇뉴진 <68093782+zladb@users.noreply.github.com>
2022-07-23 03:09:34 +0900

    알고리즘 수업 - 깊이 우선 탐색 1

--- p_24479.py
+++ p_24479.py
@@ -1,35 +1,42 @@
 # 24479 - 알고리즘 수업 - 깊이 우선 탐색 1
 
-import numpy as np
-
 N, M, R = map(int, input().split())
 
-row, col = N + 1, N + 1
-graph = np.zeros((row, col), dtype=int)
+graph = {}
 
-for x in range(M):
-    r, c = map(int, input().split())
-    graph[r][c] = 1
-    graph[c][r] = 1
+for _ in range(M):
+    u, v = map(int, input().split())
+    if u not in graph:
+        graph[u] = [v]
+    elif v not in graph[u]:
+        graph[u].append(v)
 
-# print(graph)
+    if v not in graph:
+        graph[v] = [u]
+    elif u not in graph[v]:
+        graph[v].append(u)
 
-visited = [0] * (N + 1)
 
+def dfs(root):
+    visited = []
+    stack = [root]
 
-def dfs(f):
-    visited[f] = 1
-    print(f)
-
-    flag = 0
-    for n in range(1, N+1):
-        if graph[f][n] == 1:
-            if visited[n] == 0:
-                dfs(n)
-                flag = 1
-    if flag == 0:
+    if root not in graph:
         print(0)
         return 0
+
+    while stack:
+        pop = stack.pop()
+        if pop not in visited:
+            visited.append(pop)
+            print(pop)
+
+            if pop in graph:
+                temp = list(set(graph[pop]) - set(visited))
+                temp.sort(reverse=True)
+                stack.extend(temp)
+
+    print(0)
 
 
 dfs(R)
