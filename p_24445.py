ded678e30ebbaead906a36ed29d0ba1e5fa25edc
늇뉴진 <68093782+zladb@users.noreply.github.com>
2022-07-23 19:03:41 +0900

    알고리즘 수업 - 너비 우선 탐색 2

---
+++ p_24445.py
@@ -0,0 +1,49 @@
+# 24444 - 알고리즘 수업 - 너비 우선 탐색 2
+import sys
+from collections import deque
+
+N, M, R = map(int, sys.stdin.readline().split())
+
+graph = {}
+
+for _ in range(M):
+    u, v = map(int, sys.stdin.readline().split())
+    if u not in graph:
+        graph[u] = [v]
+    else:
+        graph[u].append(v)
+
+    if v not in graph:
+        graph[v] = [u]
+    else:
+        graph[v].append(u)
+
+for K in graph:
+    graph[K].sort(reverse=True)
+
+# print(graph)
+
+def bfs(root):
+    cnt = 1
+    visited = {}
+    queue = deque([root])
+
+    while queue:
+        pop = queue.popleft()
+        if pop not in visited:
+            visited[pop] = cnt
+            cnt += 1
+
+            if pop in graph:
+                queue += graph[pop]
+
+
+    # 출력
+    for i in range(1, N+1):
+        if i in visited:
+            print(visited[i])
+        else:
+            print(0)
+
+
+bfs(R)
