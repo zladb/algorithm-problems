4c167e6e3602d7d5bc22fedd376793857e579332
늇뉴진 <68093782+zladb@users.noreply.github.com>
2022-07-25 23:28:54 +0900

    토마토

--- p_7576.py
+++ p_7576.py
@@ -17,45 +17,28 @@ for _ in range(n):
 dx = [1, -1, 0, 0]
 dy = [0, 0, -1, 1]
 
+queue = deque()
 
-def bfs(root):
-    visited = []
-    queue = deque([root])
 
+def bfs():
     while queue:
-        x, y, day = queue.popleft()
-        if (x, y) not in visited:
-            visited.append((x, y))
-            if tomato_map[x][y] == 0 or tomato_map[x][y] >= day:
-                tomato_map[x][y] = day
-                for k in range(4):
-                    nx, ny = x + dx[k], y + dy[k]
-                    if (nx, ny) not in visited:
-                        if 0 <= nx < n and 0 <= ny < m and tomato_map[nx][ny] != -1:
-                            queue.append((nx, ny, day+1))
+        x, y = queue.popleft()
+        for k in range(4):
+            nx, ny = x + dx[k], y + dy[k]
+            if 0 <= nx < n and 0 <= ny < m and tomato_map[nx][ny] == 0:
+                tomato_map[nx][ny] = tomato_map[x][y] + 1
+                queue.append((nx, ny))
 
 
 for i in range(n):
     for j in range(m):
         if tomato_map[i][j] == 1:
-            bfs((i, j, 1))
+            queue.append((i, j))
+
+bfs()
 
 # for tomato in tomato_map:
 #     print(*tomato, sep=' ')
-
-# flag = False
-# max_day = tomato_map[0][0]
-# for i in range(n):
-#     for j in range(m):
-#         if tomato_map[i][j] == 0:
-#             flag = True
-#             max_day = 0
-#             break
-#         if tomato_map[i][j] > max_day:
-#             max_day = tomato_map[i][j]
-#     if flag:
-#         break
-
 
 max_day = 0
 
@@ -63,8 +46,6 @@ for tomato in tomato_map:
     if 0 in tomato:
         max_day = 0
         break
-    if max_day < max(tomato):
-        max_day = max(tomato)
+    max_day = max(max_day, max(tomato))
 
-print(max_day-1)
-
+print(max_day - 1)
