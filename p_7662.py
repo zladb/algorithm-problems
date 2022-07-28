b563a0e1f4405d2c3a224c5f2b7df668b01f3a51
늇뉴진 <68093782+zladb@users.noreply.github.com>
2022-07-28 16:23:15 +0900

    이중 우선순위 큐

--- p_7662.py
+++ p_7662.py
@@ -3,31 +3,53 @@
 import heapq
 import sys
 
-heap = []
-cnt = {}
-
 T = int(input())
 
 for _ in range(T):
     n = int(input())
+    max_heap = []
+    min_heap = []
+    cnt = {}
+
+    def clear(heap, k=1):
+        while heap and not cnt[heap[0] * k]:
+            heapq.heappop(heap)
 
     for _ in range(n):
         a = sys.stdin.readline().split()
         command, num = a[0], int(a[1])
 
         if command == 'I':
-            heapq.heappush(heap, num)
+            heapq.heappush(max_heap, (-1) * num)
+            heapq.heappush(min_heap, num)
+            if num not in cnt:
+                cnt[num] = 1
+            else:
+                cnt[num] += 1
 
         if command == 'D':
-            if len(heap) > 0:
-                if num == 1:
-                    heap = heapq.nlargest(len(heap), heap)[1:]
-                    heapq.heapify(heap)
-                elif num == -1:
-                    heapq.heappop(heap)
+            if num == 1:
+                clear(max_heap, -1)
+                if not max_heap:
+                    continue
+                cnt[-max_heap[0]] -= 1
+                if not cnt[-max_heap[0]]:
+                    heapq.heappop(max_heap)
 
-    if len(heap) == 0:
-        print('EMPTY')
+            elif num == -1:
+                clear(min_heap)
+                if not min_heap:
+                    continue
+                cnt[min_heap[0]] -= 1
+                if not cnt[min_heap[0]]:
+                    heapq.heappop(min_heap)
+
+
+
+    clear(min_heap)
+    clear(max_heap, -1)
+
+    if min_heap and max_heap:
+        print(-max_heap[0], min_heap[0])
     else:
-        # print(heapq.nlargest(1,heap)[0], heap)
-        print(max(heap), min(heap))
+        print('EMPTY')
